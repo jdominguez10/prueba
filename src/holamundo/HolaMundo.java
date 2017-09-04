5e144dbf8d08532772dd801dd57b49d953be9d82
practica <practica@pc-14-132.etsii>
2017-09-04 12:27:15 +0100

    Modificado holamundo

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -3,7 +3,7 @@ package holamundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Pablo");
+		System.out.println("Hola Pablo, gracias por tu colaboración");
 
 	}
 
